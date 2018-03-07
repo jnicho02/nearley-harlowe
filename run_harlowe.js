378edddcea9377fe4f9d0b9d4eaddb04ead5ccfb
Jez Nicholson <jez.nicholson@gmail.com>
2018-03-07 16:27:35 +0000

    set to boolean or value

--- run_harlowe.js
+++ run_harlowe.js
@@ -3,7 +3,7 @@ const grammar = require("./harlowe.js")
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
 
-parser.feed("some words (set: $sparkyIsHappy to true)\n"
+parser.feed("some words (set: $name to 'sparky') (set: $sparkyIsHappy to true)\n"
 + "(if: !$sparkyIsHappy)[You failed to remodel all of Sparkys house without making Sparky unhappy. Well done]\n"
 + "other words [[go north]]\n"
 + "(if: $sparkyIsHappy)[You managed to remodel all of Sparkys house without making Sparky unhappy. Well done]\n"
